bb3894a1a8b4fc9bfd9ba5969b06c38ccef5f954
alawllm <alicja.willam@gmail.com>
2023-07-12 18:27:03 +0200

    wrote validate month and year functions

--- src/components/ageform.component.jsx
+++ src/components/ageform.component.jsx
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 import Calculator from "./calculator.component";
 import arrowIcon from '../assets/icon-arrow.svg'
 
+
 function AgeForm() {
     //react keeps the reference to the object in memory
     const [formData, setFormData] = useState({
@@ -12,18 +13,25 @@ function AgeForm() {
 
     //error states for handling errors
     //error states should be handled inside of handle submit
-    const [error, setError] = useState({
+
+    const defaultErrorState = {
         birthDay: false,
         birthMonth: false,
         birthYear: false
-    });
+    }
+    const [errors, setError] = useState(defaultErrorState);
 
-    console.log(error)
+    console.log(errors)
 
     //references
     const dayRef = useRef();
     const monthRef = useRef();
     const yearRef = useRef();
+
+    //reseting error state 
+    const resetErrorState = () => {
+        setError(defaultErrorState)
+    }
 
     //assigning the correct state
     const handleChange = (evt) => {
@@ -37,36 +45,86 @@ function AgeForm() {
                 [changedField]: newValue
             }
         })
+        //reseting error state at the end
+        resetErrorState();
     }
+
+    const validateDay = (day, month, year) => {
+        if (day > 0 && day <= 31) {
+            const longMonths = [1, 3, 5, 6, 8, 10, 12]
+            const shortMonths = [4, 7, 9, 11]
+            if (longMonths.includes(month)) {
+                return true;
+            } else if (shortMonths.includes(month)) {
+                day <= 30 ? true : false
+            } else if (month === 2) {
+                //check february
+            }
+        }
+        //in case if day is less than zero or more than current date
+        return false
+    }
+
+    const validateMonth = (month) => {
+        month > 0 && month <= 12 ? true : false
+    }
+
+    const validateYear = (year) => {
+        let currentDate = new Date()
+        let currentYear = currentDate.getFullYear()
+
+        year > 1920 && year <= currentYear ? true : false
+    }
+
+    const validateInputs = (day, month, year) => {
+        if (validateDay(day) === false) {
+            setError((prevErrors) => ({
+                ...prevErrors,
+                birthDay: true
+            }));
+        }
+        if (validateMonth(month) === false) {
+            setError((prevErrors) => ({
+                ...prevErrors,
+                birthMonth: true
+            }));
+        }
+        if (validateYear(year) === false) {
+            setError((prevErrors) => ({
+                ...prevErrors,
+                birthYear: true
+            }));
+        }
+    };
+
 
     //preventing default behaviour of the formular
     const handleSubmit = (evt) => {
         evt.preventDefault()
 
-        const fieldWithError = evt.target.name;
-        const newValue = true;
+        let birthDayInput = 23
+        let birthMonthInput = 4
+        let birthYearInput = 2004
 
-        //call validation
+        let booleanDay = validateDay(birthDayInput)
+        let booleanMonth = validateMonth(birthMonthInput)
+        let booleanYear = validateYear(birthYearInput)
+        console.log(booleanDay, booleanMonth, booleanYear)
+
         //set error states inside of validation
         //if validation ok, set new states
-        if (evt.target.validity.patternMismatch) {
-            setError(currError => {
-                currError[fieldWithError] = newValue;
-                return {
-                    ...currError,
-                    [fieldWithError]: newValue
-                }
-            })
-        }
+        // if (evt.target.validity.patternMismatch) {
+        //     setError(currError => {
+        //         currError[fieldWithError] = newValue;
+        //         return {
+        //             ...currError,
+        //             [fieldWithError]: newValue
+        //         }
+        //     })
+        // }
+
     }
 
-    // const validateInput = () => {
-    //     const fields = [
-    //         name: 'birthDay',
-    //         value: state.birthDay,
-    //         message:
-    //     ]
-    // }
 
     //handling blur - setting error to true
     // const handleBlur = (evt) => {
@@ -108,13 +166,14 @@ function AgeForm() {
                     inputMode="decimal"
                     // pattern="^([1-9]|[12][0-9]|3[01])$"
                     pattern="[0-9]"
-                    style={style(error)}
+                    style={style(errors)}
                     value={formData.birthDay}
                     onChange={handleChange}
                     ref={dayRef}
+                    required
                 />
                 {/* //if the error exists, the paragraph will be shown */}
-                {error.birthDay && (
+                {errors.birthDay && (
                     <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
                         Must be a valid day
                     </p>
@@ -128,12 +187,13 @@ function AgeForm() {
                     inputMode="decimal"
                     // pattern="^([1-9]|1[012])$"
                     pattern="[0-9]*"
-                    style={style(error)}
+                    style={style(errors)}
                     value={formData.birthMonth}
                     onChange={handleChange}
                     ref={monthRef}
+                    required
                 />
-                {error.birthMonth && (
+                {errors.birthMonth && (
                     <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
                         Must be a valid month
                     </p>
@@ -146,18 +206,19 @@ function AgeForm() {
                     placeholder="year"
                     inputMode="decimal"
                     pattern="[0-9]*"
-                    style={style(error)}
+                    style={style(errors)}
                     value={formData.birthYear}
                     onChange={handleChange}
                     ref={yearRef}
+                    required
                 />
-                {error.birthYear && (
+                {errors.birthYear && (
                     <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
                         Must be in the past
                     </p>
                 )}
                 <button type="submit" onClick={handleSubmit}>
-                    <img src={arrowIcon} alt="arrow icon" />
+                    <img src={arrowIcon} alt="arrow icon" className="arrow-icon" />
                 </button>
             </form >
 
