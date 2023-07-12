e20c7fbd70afed33852538b9462b364da84d9369
alawllm <alicja.willam@gmail.com>
2023-07-12 12:03:22 +0200

    error handling in the input fields

--- src/components/ageform.component.jsx
+++ src/components/ageform.component.jsx
@@ -9,62 +9,114 @@ function AgeForm() {
         birthYear: ''
     })
 
-    const [inputError, setInputError] = useState({
-        errorDay: 'Must be a valid day',
-        errorMonth: 'Must be a valid month',
-        errorMonth: 'Must be in the past'
-    })
+    const [error, setError] = useState(false);
 
-    // const updateBirthDay = (evt) => {
-    //     setFormData(evt.target.value)
-    // }
+    const handleChange = (evt) => {
+        const changedField = evt.target.name;
+        const newValue = evt.target.value;
+        //input error needs to be stored in the state
+        //if (isNaN(newValue)) setInputError('Input must be a number')
 
-    // const updateBirthMonth = (evt) => {
-    //     setFormData(evt.target.value)
-    // }
-
-    // const updateBirthYear = (evt) => {
-    //     setFormData(evt.target.value)
-    // }
-
-
-    // const handleChange = (evt) => {
-    //     const changedField = evt.target.name;
-    //     const newValue = evt.target.value;
-    //     //input error needs to be stored in the state
-    //     //if (isNaN(newValue)) setInputError('Input must be a number')
-
-    //     setFormData(currData => {
-    //         currData[changedField] = newValue;
-    //         return {
-    //             //spread operator makes a copy - different object in memory
-    //             ...currData,
-    //             //updating the value
-    //             //square brackets - variable
-    //             [changedField]: newValue
-    //         }
-    //     })
-    // }
+        setFormData(currData => {
+            currData[changedField] = newValue;
+            return {
+                //spread operator makes a copy - different object in memory
+                ...currData,
+                //updating the value
+                //square brackets - variable
+                [changedField]: newValue
+            }
+        })
+    }
 
     const handleSubmit = (evt) => {
         evt.preventDefault()
     }
+
+    const handleBlur = (evt) => {
+        //indicates whether the user has entered a value that does not satisfy the pattern
+        //attr value
+        if (evt.target.validity.patternMismatch) {
+            setError(true)
+            console.log(error)
+        }
+    }
+
+    //styled error does not show
+    function style(error) {
+        if (error) {
+            return {
+                backgroundColor: "rgba 255,0,0,0.5"
+            }
+        }
+    }
+
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <h1>{formData.birthDay} {formData.birthMonth} {formData.birthYear}</h1>
                 <label htmlFor="day">Day</label>
-                <input type="text" placeholder="day" value={formData.birthDay} onChange={updateBirthDay} id="birthday" name="birthDay" />
+                <input type="text"
+                    id="birthday"
+                    name="birthDay"
+                    placeholder="day"
+                    inputMode="decimal"
+                    // pattern="^([1-9]|[12][0-9]|3[01])$"
+                    pattern="[0-9]"
+                    style={style(error)}
+                    value={formData.birthDay}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {error && (
+                    <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
+                        Must be a valid day
+                    </p>
+                )}
 
                 <label htmlFor="month">Month</label>
-                <input type="text" placeholder="month" value={formData.birthMonth} onChange={updateBirthMonth} id="birthmonth" name="birthMonth" />
+                <input type="text"
+                    id="birthmonth"
+                    name="birthMonth"
+                    placeholder="month"
+                    inputMode="decimal"
+                    // pattern="^([1-9]|1[012])$"
+                    pattern="[0-9]*"
+                    style={style(error)}
+                    value={formData.birthMonth}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {error && (
+                    <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
+                        Must be a valid month
+                    </p>
+                )}
                 <label htmlFor="year" placeholder="">Year</label>
 
-                <input type="text" placeholder="year" value={formData.birthYear} onChange={updateBirthYear} id="birthyear" name="birthYear" />
+                <input type="text"
+                    id="birthyear"
+                    name="birthYear"
+                    placeholder="year"
+                    inputMode="decimal"
+                    pattern="[0-9]*"
+                    style={style(error)}
+                    value={formData.birthYear}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {error && (
+                    <p role="alert" style={{ color: "rgb(255, 0, 0)" }}>
+                        Must be in the past
+                    </p>
+                )}
                 <button type="submit">Submit</button>
             </form >
 
-            <Calculator day={formData.birthDay} month={formData.birthMonth} year={formData.birthYear} />
+            <Calculator
+                day={formData.birthDay}
+                month={formData.birthMonth}
+                year={formData.birthYear} />
         </>
     )
 }
--- src/components/calculator.component.jsx
+++ src/components/calculator.component.jsx
@@ -1,19 +1,22 @@
 function calculateAge(birthDay, birthMonth, birthYear) {
-    //current date
     let currentDate = new Date()
     let currentDay = currentDate.getDate()
     let currentMonth = currentDate.getMonth() + 1
     let currentYear = currentDate.getFullYear()
     //subtract given birth day from the current date
-    let ageYears = currentYear - parseFloat(birthYear);
-    let ageMonths = currentMonth - parseFloat(birthMonth);
-    let ageDays = currentDay - parseFloat(birthDay)
-    console.log(birthYear, birthMonth, birthDay)
-    console.log(ageYears, ageMonths, ageDays)
+    let ageYears = currentYear - birthYear;
+    let ageMonths = currentMonth - birthMonth;
+    let ageDays = currentDay - birthDay
+    console.log(`years: ${ageYears} months: ${ageMonths} days: ${ageDays}`)
     return ageYears, ageMonths, ageDays
 }
 
-function Calculator({ birthDay, birthMonth, birthYear }) {
+function Calculator({ day, month, year }) {
+    console.log(day, month, year)
+    let birthDay = parseInt(day)
+    let birthMonth = parseInt(month)
+    let birthYear = parseInt(year)
+    console.log(birthDay, birthMonth, birthYear)
     calculateAge(birthDay, birthMonth, birthYear)
 
     return (
